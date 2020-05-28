150085717cb64435bfd32434072c8fb7d5c9b3b6
itsalicelee <43420307+itsalicelee@users.noreply.github.com>
2020-05-29 02:08:00 +0800

    import game and menu

--- src/main.py
+++ src/main.py
@@ -1,22 +1,29 @@
 import pygame
 import sys
-import random
-import math
 from pygame.locals import *
-
-import const
-from renderer import Renderer
-from sound import Sound
-from bullet import Bullet
-from brick import Brick
-from menu import Menu
+import game
+import menu
 from game import Game
+from menu import Menu
 
 # #######################
+# class Game:
 
+# 	def __init__(self):
+# 		pygame.init()
+# 		self.renderer = Renderer()
+# 		self.menu = Menu()
+# 		self.menu.main_menu()
+# 		self.mainClock.tick(60)
+
+# 	def update(self):
+# 	 	self.mainClock.tick(60)
+# 	 	pygame.display.update()
 
 
 # ######################
 if __name__ == "__main__":
-    Game()
+	Menu()
+
+
 
