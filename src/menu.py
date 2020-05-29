2e35086ccbacc2c457600c93dff07d9a1b7ea02f
itsalicelee <43420307+itsalicelee@users.noreply.github.com>
2020-05-29 20:39:57 +0800

    fullscreen menu

--- src/menu.py
+++ src/menu.py
@@ -17,43 +17,13 @@ class Menu():
         self.main_menu()
         
 
-        #self.sound = Sound() 
-        
-
     def main_menu(self):
         button = 0
         while True:
             pygame.mouse.set_visible(False)  # 隱藏原本的游標
             self.renderer.screen.fill(const.color["black"])  # 背景底色為黑色
-            self.renderer.draw_text('main menu', self.renderer.font, const.color["white"], self.renderer.screen, 500, 200) # 畫上text，位置設定在(500,200)
+            self.renderer.draw_text('main menu', self.renderer.font, const.color["white"], self.renderer.screen, const.screen_width/2, const.screen_height/5) # 畫上text，位置設定在螢幕的中間
             
-
-            # 取得滑鼠的座標，回傳一個tuple(x,y)
-            # mx, my = pygame.mouse.get_pos() 
-
-            # if  self.renderer.button_1.collidepoint((mx, my)):  # 若游標在button_1內
-            #     if self.click:  # 若有點擊，進入遊戲
-            # #       game.game_start()
-            #         pass
-            # if  self.renderer.button_2.collidepoint((mx, my)):  # 若游標在button_2內
-            #     if self.click:  # 若有點擊，進入遊戲
-            #       self.options()  # 進入option
-      
-            # if 400 < mx < 600 and 500 < my < 550:  # 如果游標移到第一個選單方框內
-            #     pygame.draw.rect(self.renderer.screen, const.color["red"],  self.renderer.button_1)  # 畫出紅色矩形
-            # else:
-            #     pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_1)  # 畫上藍色矩形，傳入畫布、顏色、矩形
-
-            # if 400 < mx < 600 and 600 < my < 650:  # 如果游標移到第二個選單方框內
-            #     pygame.draw.rect(self.renderer.screen, const.color["red"], self.renderer.button_2)  # 畫出紅色矩形
-            # else:
-            #     pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_2)  # 畫上藍色矩形，傳入畫布、顏色、矩形
-
-            
-            #self.renderer.draw_text('option', self.renderer.font, const.color["white"], self.renderer.screen, 500, 625) # 在矩形上加上option的text
-            #self.renderer.draw_text('start', self.renderer.font, const.color["white"], self.renderer.screen, 500, 525)  # 在矩形上加上start的text
-            
-            #self.click = False
             pygame.draw.rect(self.renderer.screen, const.color["red"],  self.renderer.button_1)  # 一開始預設畫出start紅色矩形
             pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_2)  # 畫上藍色矩形，傳入畫布、顏色、矩形
             pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_3)  # 畫上藍色矩形，傳入畫布、顏色、矩形
@@ -62,10 +32,6 @@ class Menu():
             
             for event in pygame.event.get():
 
-                #game.event_handler(self, event)
-                # if event.type == pygame.QUIT:
-                #     pygame.quit()
-                #     sys.exit()
                 if event.type == pygame.KEYDOWN:  # 若按下esc鍵，退出遊戲
 
                     if event.key == const.key["down"]:  # 若按下down
@@ -95,11 +61,6 @@ class Menu():
                 pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_2)  # 畫上藍色矩形，傳入畫布、顏色、矩形
                 pygame.draw.rect(self.renderer.screen, const.color["red"], self.renderer.button_3)  # 畫上藍色矩形，傳入畫布、顏色、矩形
             
-
-
-
-
-            
             # 畫矩形以及字
             #pygame.draw.rect(self.renderer.screen, const.color["red"],  self.renderer.button_1)  # 一開始預設畫出start紅色矩形
             self.renderer.draw_text('start', self.renderer.font, const.color["white"], self.renderer.screen, const.menuButton["game"][0]+0.5*(const.menuButton["game"][2]), const.menuButton["game"][1]+0.5*(const.menuButton["game"][3]))  # 在矩形上加上start的text
@@ -115,18 +76,6 @@ class Menu():
 
             pygame.display.update()  # 更新畫布
 
-    # def mouse_position(self):
-    #     mx, my = pygame.mouse.get_pos()  # 取得滑鼠的座標，回傳一個tuple(x,y)
-
-    #     if 400 < mx < 600 and 500 < my < 550:  # 如果游標移到第一個選單方框內
-    #         pygame.draw.rect(Renderer.screen, const.color["red"],  Renderer.button_1)  # 畫出紅色矩形
-    #     else:
-    #         pygame.draw.rect(Renderer.screen, const.color["blue"], Renderer.button_1)  # 畫上藍色矩形，傳入畫布、顏色、矩形
-
-    #     if 400 < mx < 600 and 600 < my < 650:  # 如果游標移到第二個選單方框內
-    #         pygame.draw.rect(Renderer.screen, const.color["red"], Renderer.button_2)  # 畫出紅色矩形
-    #     else:
-    #         pygame.draw.rect(Renderer.screen, const.color["blue"], Renderer.button_2)  # 畫上藍色矩形，傳入畫布、顏色、矩形
 
     def options(self):
         button = 0
@@ -134,7 +83,7 @@ class Menu():
         running = True #遊戲選項
         while running:
             self.renderer.screen.fill(const.color["black"])  # 設定option背景顏色
-            self.renderer.draw_text('options', self.renderer.font, const.color["white"], self.renderer.screen, 500, 50)
+            self.renderer.draw_text('options', self.renderer.font, const.color["white"], self.renderer.screen,  const.screen_width/2, const.screen_height/4)
 
             pygame.draw.rect(self.renderer.screen, const.color["red"],  self.renderer.button_1)  # 一開始預設畫出start紅色矩形
             pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_2)  # 畫上藍色矩形，傳入畫布、顏色、矩形
@@ -188,7 +137,7 @@ class Menu():
         
         while True:
             self.renderer.screen.fill(const.color["black"])  # 設定option背景顏色
-            self.renderer.draw_text('volume', self.renderer.font, const.color["white"], self.renderer.screen, 500, 50)
+            self.renderer.draw_text('volume', self.renderer.font, const.color["white"], self.renderer.screen,  const.screen_width/2, const.screen_height/4)
             
 
             for event in pygame.event.get():
@@ -209,6 +158,8 @@ class Menu():
                     if event.key == const.key["esc"]:    # 若按下option，回到選單
                         self.options()
                     
-            self.renderer.draw_text('set volume', self.renderer.font, const.color["white"], self.renderer.screen, 500, 200)
+            self.renderer.draw_text('set volume', self.renderer.font, const.color["white"], self.renderer.screen,  const.screen_width/2, const.screen_height/5)
             
             pygame.display.update()
+
+            
