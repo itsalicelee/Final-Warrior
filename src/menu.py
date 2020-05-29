61479fbc7bc6ad82149efdd4e6e479a6725f793d
張祥賢 <zhangxiangxian@zhangxiangxiandeMacBook-Pro.local>
2020-05-29 17:34:19 +0800

    modify menu

--- src/menu.py
+++ src/menu.py
@@ -2,27 +2,32 @@ import const
 from renderer import Renderer
 import pygame, sys
 from game import Game
-import sound
+from sound import Sound
 
 class Menu():
     """docstring for Menu"""
+    
 
     def __init__(self):
         pygame.init()
         self.click = False
         self.renderer = Renderer()
         self.game = Game()
+        self.bgm = Sound()
         self.main_menu()
+        
+
+        #self.sound = Sound() 
         
 
     def main_menu(self):
         button = 0
         while True:
-            pygame.mouse.set_visible(False)  # 隱藏原本的游標
 
+            pygame.mouse.set_visible(False)  # 隱藏原本的游標
             self.renderer.screen.fill(const.color["black"])  # 背景底色為黑色
             self.renderer.draw_text('main menu', self.renderer.font, const.color["white"], self.renderer.screen, 500, 200) # 畫上text，位置設定在(500,200)
-
+            
 
             # 取得滑鼠的座標，回傳一個tuple(x,y)
             # mx, my = pygame.mouse.get_pos() 
@@ -125,23 +130,85 @@ class Menu():
     #         pygame.draw.rect(Renderer.screen, const.color["blue"], Renderer.button_2)  # 畫上藍色矩形，傳入畫布、顏色、矩形
 
     def options(self):
-        
+        button = 0
+
         running = True #遊戲選項
         while running:
             self.renderer.screen.fill(const.color["black"])  # 設定option背景顏色
             self.renderer.draw_text('options', self.renderer.font, const.color["white"], self.renderer.screen, 500, 50)
 
+            pygame.draw.rect(self.renderer.screen, const.color["red"],  self.renderer.button_1)  # 一開始預設畫出start紅色矩形
+            pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_2)  # 畫上藍色矩形，傳入畫布、顏色、矩形
+            pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_3)  # 畫上藍色矩形，傳入畫布、顏色、矩形
+
+
+            if button % 3 == 0:  # 選到start
+                pygame.draw.rect(self.renderer.screen, const.color["red"],  self.renderer.button_1)  # 一開始預設畫出start紅色矩形
+                pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_2)  # 畫上藍色矩形，傳入畫布、顏色、矩形
+                pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_3)  # 畫上藍色矩形，傳入畫布、顏色、矩形
+
+            if button % 3 == 1:  # 選到option 
+                pygame.draw.rect(self.renderer.screen, const.color["blue"],  self.renderer.button_1)  # 一開始預設畫出start紅色矩形
+                pygame.draw.rect(self.renderer.screen, const.color["red"], self.renderer.button_2)  # 畫上藍色矩形，傳入畫布、顏色、矩形
+                pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_3)  # 畫上藍色矩形，傳入畫布、顏色、矩形
+            if button % 3 == 2:  # 選到quit
+                pygame.draw.rect(self.renderer.screen, const.color["blue"],  self.renderer.button_1)  # 一開始預設畫出start紅色矩形
+                pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_2)  # 畫上藍色矩形，傳入畫布、顏色、矩形
+                pygame.draw.rect(self.renderer.screen, const.color["red"], self.renderer.button_3)  # 畫上藍色矩形，傳入畫布、顏色、矩形
+            
+
+            self.renderer.draw_text('volume', self.renderer.font, const.color["white"], self.renderer.screen, const.menuButton["game"][0]+0.5*(const.menuButton["game"][2]), const.menuButton["game"][1]+0.5*(const.menuButton["game"][3]))  # 在矩形上加上start的text
+
+            #pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_2)  # 畫上藍色矩形，傳入畫布、顏色、矩形
+            self.renderer.draw_text('theme', self.renderer.font, const.color["white"], self.renderer.screen, const.menuButton["option"][0]+0.5*(const.menuButton["option"][2]), const.menuButton["option"][1]+0.5*(const.menuButton["option"][3])) # 在矩形上加上option的text
+
+            #pygame.draw.rect(self.renderer.screen, const.color["blue"], self.renderer.button_3)  # 畫上藍色矩形，傳入畫布、顏色、矩形
+            self.renderer.draw_text('main menu', self.renderer.font, const.color["white"], self.renderer.screen, const.menuButton["quit"][0]+0.5*(const.menuButton["quit"][2]), const.menuButton["quit"][1]+0.5*(const.menuButton["quit"][3])) # 在矩形上加上option的text          
+
+
 
             for event in pygame.event.get():
-
-                # if event.type == pygame.QUIT:  # 關閉視窗
-                #     pygame.quit()
-                #     sys.exit()
                 if event.type == pygame.KEYDOWN:  # 若按下ESC鍵，退出option
                     if event.key == pygame.K_ESCAPE:
                         self.main_menu()
+                    if event.key == const.key["down"]:  # 若按下down
+                        button += 1
+                    if event.key == const.key["up"]:
+                        button -= 1
+                    if event.key == const.key["space"]:
+                        if button % 3 == 0:  # 進入volume
+                            self.volume()
+                        if button % 3 == 1:  # 進入theme
+                            pass  
+                        if button % 3 ==  2:  # 回到main menu
+                            self.main_menu()
 
             pygame.display.update()
+
+    def volume(self):
+        
+        while True:
+            self.renderer.screen.fill(const.color["black"])  # 設定option背景顏色
+            self.renderer.draw_text('volume', self.renderer.font, const.color["white"], self.renderer.screen, 500, 50)
+
+            for event in pygame.event.get():   
+                if event.type == pygame.KEYDOWN:  # 若按下ESC鍵，退出option
+                    if event.key == const.key["left"]:  # 若按下left，音量減小
+                        self.bgm.set_bgm(self.bgm.get_volume()-0.1)
+
+                    if event.key == const.key["right"]:  # 若按下right，音量增大
+                        self.bgm.set_bgm(self.bgm.get_volume()+0.1)
+                            
+                    if event.key == const.key["esc"]:
+                        self.options()
+
+            self.renderer.draw_text('set', self.renderer.font, const.color["white"], self.renderer.screen, 500, 200)
+            
+            pygame.display.update()
+
+        
+
+
 
 
             
