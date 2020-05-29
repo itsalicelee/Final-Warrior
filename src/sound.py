71cd46d2838c6dad69f587c510ed9f826407e102
itsalicelee <43420307+itsalicelee@users.noreply.github.com>
2020-05-29 17:38:45 +0800

    good bgm

--- src/sound.py
+++ src/sound.py
@@ -6,12 +6,17 @@ class Sound:
 		pygame.mixer.init()
 
 		# 讀取背景音樂
-		self.bgm = pygame.mixer.music.load("music/bgm.ogg")
-		self.play_bgm()
+		pygame.mixer.music.load("music/bgm.ogg")
+		pygame.mixer.music.set_volume(float(0.3))  # 來設定播放的音量，音量value的範圍為0.0到1.0
+		pygame.mixer.music.play(-1)
+		
 
-	def play_bgm(self):
-		pygame.mixer.music.set_volume(0.1)  # 來設定播放的音量，音量value的範圍為0.0到1.0
-		pygame.mixer.music.play(-1)  # 若為-1則會無限輪迴播放
+	def set_bgm(self, value):
+		pygame.mixer.music.set_volume(value)  # 來設定播放的音量，音量value的範圍為0.0到1.0
+
+
+	def get_volume(self):
+		return pygame.mixer.music.get_volume()
 
 
 
