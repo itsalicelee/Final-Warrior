a2143f602ecadd783c13e2dff34ae126f3ec7120
jeffery3756 <55586934+jeffery3756@users.noreply.github.com>
2020-05-30 14:39:26 +0800

    Update renderer.py

--- src/renderer.py
+++ src/renderer.py
@@ -18,7 +18,8 @@ class Renderer:
         self.map_height = self.photo_dct["bg"].get_height()
 
         self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)  # 顯示視窗
-
+        const.screen_width = self.screen.get_width()
+        const.screen_height = self.screen.get_height()
 ##########################################################################################
 # 各種按鈕們
         # 建立矩形_1，長200寬50，距離左邊邊界為400，距離上面邊界400
@@ -67,26 +68,59 @@ class Renderer:
     def rolling_map(self, role_x, role_y):
         self.role_x = role_x
         self.role_y = role_y
+        self.x_change = 0
+        self.y_change = 0
         # x 方向
         if role_x < const.screen_width / 2:
             const.map_x = 0
 
         elif role_x > self.map_width - const.screen_width / 2:
-            const.map_x = -(self.map_width - const.screen_width)
+            self.map_x = -(self.map_width - const.screen_width)
+            self.x_change = self.map_x - const.map_x
+            const.map_x = self.map_x
 
         else:
-            const.map_x =  -(role_x - const.screen_width / 2)
+            self.map_x =  -(role_x - const.screen_width / 2)
+            self.x_change = self.map_x - const.map_x
+            const.map_x = self.map_x
 
           # y 方向
         if role_y < const.screen_height / 2:
             const.map_y = 0
 
         elif role_y > self.map_height - const.screen_height / 2:
-            const.map_y = -(self.map_height - const.screen_height)
+            self.map_y = -(self.map_height - const.screen_height)
+            self.y_change = self.map_y - const.map_y
+            const.map_y = self.map_y
 
         else:
-            const.map_y = -(role_y - const.screen_height / 2)
+            self.map_y = -(role_y - const.screen_height / 2)
+            self.y_change = self.map_y - const.map_y
+            const.map_y = self.map_y
+        return self.x_change, self.y_change
     
+
+    # def game_pause(self):
+    #     # 將 background 顯示在screen上
+    #     self.screen.blit(self.photo_dct["bg"], (const.map_x, const.map_y))
+
+    #     # 將主角顯示在screen上
+    #     self.screen.blit(self.photo_dct["actorIMG"], (int(const.map_x + const.now_x), int(const.map_y + const.now_y)))
+    #     # print(self.now_x, self.now_y)
+
+    #     self.draw_hp()
+    #     self.draw_pasue_button()
+
+    #     self.draw_text("pause", self.font, const.color["black"], self.screen, const.screen_width/2, const.screen_height/2)
+
+    #     # 螢幕更新
+    #     pygame.display.update()
+
+
+
+
+
+
 
 
     # 先畫出hp的位置，之後要改成 hp 血條的圖片
