e18011a2d7c1802155466364b11ac0841dce10e9
jeffery3756 <55586934+jeffery3756@users.noreply.github.com>
2020-05-31 22:40:42 +0800

    Update boss.py

--- src/boss.py
+++ src/boss.py
@@ -1,12 +1,27 @@
-from game_object import Object
-class Boss(Object):
-	"""docstring for boss"""
+import pygame
+class Boss(pygame.sprite.Sprite):
 	def __init__(self, x, y, hp = 100):
-		super(Boss, self).__init__(x, y)
+		pygame.sprite.Sprite.__init__(self)
 		self.hp = hp
+		self.alive = True
+		self.image = pygame.image.load("images/bad.png")
+		pygame.transform.scale(self.image,(80,60))
+		self.x = x
+		self.y = y
+		self.set_properties(x, y)
 
+	def set_properties(self, x, y):
+		self.rect = self.image.get_rect()
+		self.rect.centerx = x
+		self.rect.centery = y
 
-		
-	def attack():
-		pass
-		
+	def get_loc(self):
+		return self.rect.x, self.rect.y
+	 
+	def be_attacked(self):
+		self.hp -= 1
+		if self.hp <= 0:
+			self.alive = False
+	
+	def if_still_alive(self):
+		return self.alive
