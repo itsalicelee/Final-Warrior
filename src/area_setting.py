8c4344508c9047d8c7085e7b2c1ac92afd0623e9
johnny890122 <54676778+johnny890122@users.noreply.github.com>
2020-06-05 22:51:17 +0800

    Update area_setting.py

--- src/area_setting.py
+++ src/area_setting.py
@@ -12,13 +12,13 @@ block_up = Block(0, 61 ,2000, 1)
 block_down = Block(0, 1200, 2000, 1)
 
 # 左方邊界
-block_left = Block(88, 0, 1, 1300)
+block_left = Block(65, 0, 1, 1300)
 
 # 右方邊界
 block_right = Block(1800, 0, 1, 1300)
 
 block_left_and_down = Block(0, 1150, 793, 50)
-block_right_and_down = Block(1097, 1150, 793, 50)
+block_right_and_down = Block(1100, 1150, 793, 50)
 
 
 
